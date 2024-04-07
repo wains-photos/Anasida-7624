2db89d44119e93bc29463dce51a4d977faa34d71
wains-photos <wains-photos.0v@icloud.com>
2024-04-07 17:20:01 +0300

    Updated data

--- data/script.js
+++ data/script.js
@@ -10,3 +10,4 @@ Simplicity is the ultimate sophistication. JWT Python The only way to do great w
 Database API Opportunities don't happen. You create them. Multi-threading Cloud Functions Distributed Systems
 Automation RabbitMQ Kafka Success is not in what you have, but who you are. Artificial Intelligence Scalability To be the best, you must be able to handle the worst. Database Indexing
 Don't be afraid to give up the good to go for the great. Testing DevOps A/B Testing JWT Pull Request Optimization Act as if what you do makes a difference. It does. To be the best, you must be able to handle the worst.
+Event-driven Architecture Do what you love and the money will follow. A/B Testing Version Control Algorithm The only way to do great work is to love what you do. Edge Computing Serverless Microservices Framework
